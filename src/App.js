818af04134f6a5842b759c3d5ba4d1f807b2e16c
amlegleiter@mail.fhsu.edu <amlegleiter@mail.fhsu.edu>
2021-12-18 20:45:39 -0500

    style changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react'
 import BannerTop from './sections/bannerTop/bannerTop.js';
+import SocialMedia from './sections/socialMedia/socialMedia.js';
 import Hero from './sections/hero/hero.js'
+import Portfolio from './sections/portfolio/portfolio.js';
+import TechCloud from './sections/techCloud/techCloud.js';
+import ContactMe from './sections/contactMe/contactMe.js';
+import Footer from './sections/footer/footer.js';
 
 
 let App = () => {
@@ -9,8 +14,12 @@ let App = () => {
     <>
       <BannerTop />
       <Hero />
+      <TechCloud />
+      <Portfolio />
+      <SocialMedia />
+      <ContactMe />
+      <Footer />
     </>
-
   )
 }
 
